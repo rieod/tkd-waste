5dcba04a63e4d365295693919415409efe01f28c
sariwahyu <124552096+rieod@users.noreply.github.com>
2024-06-05 15:18:42 +0700

    frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import PrivateRoute from './utils/PrivateRoute'
+import { AuthProvider } from './context/AuthContext'
+
+import Homepage from './views/Homepage'
+import Registerpage from './views/Registerpage'
+import Loginpage from './views/Loginpage'
+import Dashboard from './views/Dashboard'
+import Navbar from './views/Navbar'
+
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider>
+        <Navbar />
+      </AuthProvider>
+    </Router>
+  );
+}
+
+export default App
